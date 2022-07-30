66439e7a2c0ec37aa2919d2acdb1bd1f685e3628
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-30 15:49:43 +0700

    Make CaseStudy

---
+++ src/Model/Customer/Customer.java
@@ -0,0 +1,6 @@
+package Model.Customer;
+
+public class Customer {
+    private String name;
+    private String ;
+}
---
+++ src/Model/Product/Product.java
@@ -0,0 +1,8 @@
+package Model.Product;
+
+public class Product {
+    private String ID;
+    private String name;
+    private double price;
+    private
+}
